c421f9515c8fd8dc757d62cf7d0e2ac80dd48d12
Denis Negura <den.negura@gmail.com>
2026-04-05 13:43:36 +0300

    persist was added

--- src/store/useCounterStore.js
+++ src/store/useCounterStore.js
@@ -1,7 +1,7 @@
 import {create} from "zustand";
-import {devtools} from 'zustand/middleware';
+import {createJSONStorage, devtools, persist} from 'zustand/middleware';
 
-const initialState = {count: 0}
+const initialState = {count: 0, something: 'something'}
 
 const counterStore = (set) => ({
     ...initialState,
@@ -10,5 +10,16 @@ const counterStore = (set) => ({
     decrement: () => set((state) => ({count: state.count - 1}), false, 'decrement'),
 });
 
-export const useCounterStore = create(devtools(counterStore))
+export const useCounterStore = create(
+    devtools(
+        persist(
+            counterStore,
+            {
+                name: 'counter-storage',
+                storage: createJSONStorage(() => localStorage),
+                partialize: (state) => ({count: state.count})
+            }
+        )
+    )
+)
 
