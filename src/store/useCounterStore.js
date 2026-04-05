d2e256c1c09a61d9b4469a54e52ff19e3435e600
Denis Negura <den.negura@gmail.com>
2026-04-05 13:52:27 +0300

    immer was added

--- src/store/useCounterStore.js
+++ src/store/useCounterStore.js
@@ -1,25 +1,25 @@
 import {create} from "zustand";
 import {createJSONStorage, devtools, persist} from 'zustand/middleware';
+import {immer} from "zustand/middleware/immer";
 
 const initialState = {count: 0, something: 'something'}
 
 const counterStore = (set) => ({
     ...initialState,
     reset: () => set({count: 0}, false, 'reset'),
-    increment: () => set((state) => ({count: state.count + 1}), false, 'increment'),
+    // без immer нужно создавать новый state
     decrement: () => set((state) => ({count: state.count - 1}), false, 'decrement'),
+    // immer сам создаст новый state
+    increment: () => set((state) => {
+        state.count += 1
+    }, false, 'increment'),
 });
 
 export const useCounterStore = create(
-    devtools(
-        persist(
-            counterStore,
-            {
-                name: 'counter-storage',
-                storage: createJSONStorage(() => localStorage),
-                partialize: (state) => ({count: state.count})
-            }
-        )
-    )
+    immer(devtools(persist(counterStore, {
+        name: 'counter-storage',
+        storage: createJSONStorage(() => localStorage),
+        partialize: (state) => ({count: state.count})
+    })))
 )
 
